463b90146ea53a31cabd17cbe94f2cef85f43b8a
Erick Veil <registry@erickveil.com>
2011-10-11 12:47:10 -0700

    SaveStr
    added SaveStr module to overwrite strings to a text file.

--- filetest.cpp
+++ filetest.cpp
@@ -1,17 +1,50 @@
 #include<fstream>
 #include<iostream>
+#include<string>
+#include<cstring>
 using namespace std;
 #define IOFILE "test.txt"
 
 int touch(void);
+int SaveStr(string str);
 
 int main(void)
 {
 	fstream file;
 	string sstring = "text to be saved.";
 
-	touch();
+	SaveStr(sstring);
 	return 0;
+}
+
+/*
+	requires: fstream,iostream,string,cstring
+*/
+int SaveStr(string str)
+{
+	int ret=1;
+	fstream file;
+
+	// dynamicaly create an array of characters from the given string
+	char* cstrloc;
+	cstrloc = new char [str.size()+1];
+	strcpy(cstrloc, str.c_str());
+
+	// open the file for writing
+	file.open(IOFILE,fstream::out);
+	if (file.fail())
+	{
+		cout<<"\nFile read\\write failed.\n";
+		ret = 0;
+	}
+	else
+	{
+		file.write(cstrloc,str.size()+1);
+	}
+	//clean up and return
+	file.close();
+	delete[] cstrloc;
+	return ret;
 }
 
 /*
