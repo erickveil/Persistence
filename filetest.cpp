05174293a73be4a81353bf3644586724404152cc
Erick Veil <registry@erickveil.com>
2011-10-11 12:10:32 -0700

    filetest touch v1
    touch-like module. throw a warning in CodeBlocks written as is. Not sure if
    I'm checking the failbit correctly, but it seemed like the intuitive way of
    doing it.

--- filetest.cpp
+++ filetest.cpp
@@ -1,13 +1,32 @@
 #include<fstream>
+#include<iostream>
 using namespace std;
+#define IOFILE "test.txt"
+
+int touch(void);
 
 int main(void)
 {
 	fstream file;
 	string sstring = "text to be saved.";
-	
-	file.open("test.txt",fstream::in|fstream::out|fstream::app);
-	file.write(sstring.c_str(),sizeof(sstring.c_str()));
-	file.close();
+
+	touch();
 	return 0;
+}
+
+/*
+	requires: fstream,iostream
+*/
+int touch(void)
+{
+	int ret=1;
+	fstream file;
+	file.open(IOFILE,fstream::in|fstream::out|fstream::app);
+	if (file.fail())
+	{
+		cout<<"\nFile read\\write failed.\n";
+		ret = 0;
+	}
+	file.close();
+	return ret;
 }
