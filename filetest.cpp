14991540cbe33b81a026be60d47d3b615486546f
Erick Veil <registry@erickveil.com>
2011-10-11 13:30:53 -0700

    Filling a Structure
    Here we grab lines from the file to fill a structure, over and over, until
    all lines are used up.
    
    Next we trya  vector of structures.

--- filetest.cpp
+++ filetest.cpp
@@ -2,31 +2,30 @@
 #include<iostream>
 #include<string>
 #include<cstring>
+#include<vector>
 using namespace std;
 #define IOFILE "test.txt"
 
 int touch(void);
 int SaveStr(string str);
-string LoadStr(void);
+int LoadStr(string* str, int position);
+
+typedef struct THING
+{
+	string str1;
+	string str2;
+	string str3;
+}THING;
+
+vector <THING> list;
 
 int main(void)
 {
 	fstream file;
 
-	string sstring = LoadStr();
-	cout<<sstring;
-
-	sstring = LoadStr();
-	cout<<sstring;
+	THING it;
 
 
-	return 0;
-}
-
-string LoadStr(void)
-{
-	fstream file;
-	string buffer;
 	file.open(IOFILE,fstream::in);
 	if (file.fail())
 	{
@@ -34,10 +33,38 @@ string LoadStr(void)
 	}
 	else
 	{
-		getline(file,buffer);
+		while(!file.eof())
+		{
+			getline(file,it.str1);
+			getline(file,it.str2);
+			getline(file,it.str3);
+			cout<<it.str1<<endl<<it.str2<<endl<<it.str3<<endl;
+		}
+	}
+
+
+	file.close();
+
+	return 0;
+}
+
+int LoadFile(string* str, int position)
+{
+	fstream file;
+	int ret = 0;
+	file.open(IOFILE,fstream::in);
+	if (file.fail())
+	{
+		cout<<"\nFile read\\write failed.\n";
+	}
+	else
+	{
+		file.seekg(position);
+		getline(file,*str);
+		ret=file.tellg();
 	}
 	file.close();
-	return buffer;
+	return ret;
 }
 
 
